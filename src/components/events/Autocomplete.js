ef52090d6158629eed4df119a65d34c143632cfb
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-06-25 15:12:54 -0700

    autocomplete kind of working

--- src/components/events/Autocomplete.js
+++ src/components/events/Autocomplete.js
@@ -1,93 +1,157 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
+import { classnames } from '../helpers';
 
-import Map, { Marker } from 'google-maps-react';
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      address: '',
+      errorMessage: '',
+      latitude: null,
+      longitude: null,
+      isGeocoding: false,
+    };
+  }
 
-import styles from './autocomplete.module.css';
-
-class Contents extends Component {
-  state = {
-    position: null
+  handleChange = address => {
+    this.setState({
+      address,
+      latitude: null,
+      longitude: null,
+      errorMessage: '',
+    });
   };
 
-  componentDidMount() {
-    this.renderAutoComplete();
-  }
+  handleSelect = selected => {
+    this.setState({ isGeocoding: true, address: selected });
+    geocodeByAddress(selected)
+      .then(res => getLatLng(res[0]))
+      .then(({ lat, lng }) => {
+        this.setState({
+          latitude: lat,
+          longitude: lng,
+          isGeocoding: false,
+        });
+      })
+      .catch(error => {
+        this.setState({ isGeocoding: false });
+        console.log('error', error); // eslint-disable-line no-console
+      });
+  };
 
-  componentDidUpdate(prevProps) {
-    if (this.props !== prevProps.map) this.renderAutoComplete();
-  }
-
-  onSubmit(e) {
-    e.preventDefault();
-  }
-
-  renderAutoComplete() {
-    const { google, map } = this.props;
-
-    if (!google || !map) return;
-
-    const autocomplete = new google.maps.places.Autocomplete(this.autocomplete);
-    autocomplete.bindTo('bounds', map);
-
-    autocomplete.addListener('place_changed', () => {
-      const place = autocomplete.getPlace();
-
-      if (!place.geometry) return;
-
-      if (place.geometry.viewport) map.fitBounds(place.geometry.viewport);
-      else {
-        map.setCenter(place.geometry.location);
-        map.setZoom(17);
-      }
-
-      this.setState({ position: place.geometry.location });
+  handleCloseClick = () => {
+    this.setState({
+      address: '',
+      latitude: null,
+      longitude: null,
     });
-  }
+  };
+
+  handleError = (status, clearSuggestions) => {
+    console.log('Error from Google Maps API', status); // eslint-disable-line no-console
+    this.setState({ errorMessage: status }, () => {
+      clearSuggestions();
+    });
+  };
 
   render() {
-    const { position } = this.state;
+    const {
+      address,
+      errorMessage,
+      latitude,
+      longitude,
+      isGeocoding,
+    } = this.state;
 
     return (
-      <div className={styles.flexWrapper}>
-        <div className={styles.left}>
-          <form onSubmit={this.onSubmit}>
-            <input
-              placeholder="Enter a location"
-              ref={ref => (this.autocomplete = ref)}
-              type="text"
-            />
+      <div>
+        <PlacesAutocomplete
+          onChange={this.handleChange}
+          value={address}
+          onSelect={this.handleSelect}
+          onError={this.handleError}
+          shouldFetchSuggestions={address.length > 2}
+        >
+          {({ getInputProps, suggestions, getSuggestionItemProps }) => {
+            return (
+              <div className="Demo__search-bar-container">
+                <div className="Demo__search-input-container">
+                  <input
+                    {...getInputProps({
+                      placeholder: 'Search Places...',
+                      className: 'Demo__search-input',
+                    })}
+                  />
+                  {this.state.address.length > 0 && (
+                    <button
+                      className="Demo__clear-button"
+                      onClick={this.handleCloseClick}
+                    >
+                      x
+                    </button>
+                  )}
+                </div>
+                {suggestions.length > 0 && (
+                  <div className="Demo__autocomplete-container">
+                    {suggestions.map(suggestion => {
+                      const className = classnames('Demo__suggestion-item', {
+                        'Demo__suggestion-item--active': suggestion.active,
+                      });
 
-            <input className={styles.button} type="submit" value="Go" />
-          </form>
+                      return (
+                        /* eslint-disable react/jsx-key */
+                        <div
+                          {...getSuggestionItemProps(suggestion, { className })}
+                        >
+                          <strong>
+                            {suggestion.formattedSuggestion.mainText}
+                          </strong>{' '}
+                          <small>
+                            {suggestion.formattedSuggestion.secondaryText}
+                          </small>
+                        </div>
+                      );
+                      /* eslint-enable react/jsx-key */
+                    })}
+                    <div className="Demo__dropdown-footer">
+                      <div>
+                      </div>
+                    </div>
+                  </div>
+                )}
+              </div>
+            );
+          }}
+        </PlacesAutocomplete>
+        {errorMessage.length > 0 && (
+          <div className="Demo__error-message">{this.state.errorMessage}</div>
+        )}
 
+        {((latitude && longitude) || isGeocoding) && (
           <div>
-            <div>Lat: {position && position.lat()}</div>
-            <div>Lng: {position && position.lng()}</div>
+            <h3 className="Demo__geocode-result-header">Geocode result</h3>
+            {isGeocoding ? (
+              <div>
+                <i className="fa fa-spinner fa-pulse fa-3x fa-fw Demo__spinner" />
+              </div>
+            ) : (
+              <div>
+                <div className="Demo__geocode-result-item--lat">
+                  <label>Latitude:</label>
+                  <span>{latitude}</span>
+                </div>
+                <div className="Demo__geocode-result-item--lng">
+                  <label>Longitude:</label>
+                  <span>{longitude}</span>
+                </div>
+              </div>
+            )}
           </div>
-        </div>
-
-        <div className={styles.right}>
-          <Map apiKey="AIzaSyDmfqYnJinAb2l15myNeWVhZEHt4xS9-4U"
-            {...this.props}
-            center={position}
-            centerAroundCurrentLocation={false}
-            containerStyle={{
-              height: '100vh',
-              position: 'relative',
-              width: '100%'
-            }}>
-            <Marker position={position} />
-          </Map>
-        </div>
+        )}
       </div>
     );
   }
 }
 
-const MapWrapper = props => (
-  <Map className="map" google={props.google} visible={false}>
-    <Contents {...props} />
-  </Map>
-);
-
-export default MapWrapper;
+export default SearchBar;
