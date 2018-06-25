5f926f4f29a5a7c77baf0f11f57b185438fa23bd
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-25 13:46:00 -0700

    added groups reducer to the store

--- src/store/store.js
+++ src/store/store.js
@@ -2,10 +2,12 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import promiseMiddleware from './promise-middleware';
 import { error, loading } from '../components/app/reducers';
+import { groups } from '../components/groups/Groups';
 
 const rootReducer = combineReducers({
   error,
-  loading
+  loading,
+  groups
 });
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
