4875dec1e37211f981384efdd1ad74230b9d41dd
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-25 13:44:38 -0700

    trying to figure out how to write and update reducer

--- src/components/groups/reducers.js
+++ src/components/groups/reducers.js
@@ -1,5 +1,6 @@
 export const GROUPS_LOAD = 'GROUPS_LOAD';
 export const GROUP_ADD = 'GROUP_ADD';
+export const GROUP_UPDATE = 'GROUP_UPDATE';
 
 
 export function groups(state = [], { type, payload }) {
@@ -10,6 +11,13 @@ export function groups(state = [], { type, payload }) {
     case GROUP_ADD: {
       return [...state, payload];
     }
+    // case GROUP_UPDATE: {
+    //   const copy = state;
+    //   const updatedGroups = copy.map(group => {
+    //     if(group._id === payload._id) group = payload;
+    //   });
+    //   return updatedGroups;
+    // }
     default:
       return state;
   }
--- src/components/groups/reducers.test.js
+++ src/components/groups/reducers.test.js
@@ -1,7 +1,8 @@
 import {
   groups,
   GROUPS_LOAD,
-  GROUP_ADD
+  GROUP_ADD,
+  GROUP_UPDATE
 } from './reducers';
 
 
@@ -35,6 +36,19 @@ describe('Groups Reducer', () => {
     expect(state).toEqual([data, newData]);
   });
 
+  // it('updates a group', () => {
+  //   const data = { _id: 1, type: 'running', teamName: 'RUNNERS!', description: 'We like to run', private: false };
+  //   const moreData = { _id: 2, type: 'basketball', teamName: 'Ballers!', description: 'We like to basketball', private: false };
+
+  //   const update = { _id: 1, type: 'running', teamName: 'WALKERs!', description: 'We like to run', private: false };
+
+  //   const state = groups([data, moreData], {
+  //     type: GROUP_UPDATE,
+  //     payload: update
+  //   });
+
+  //   expect(state).toEqual([update, moreData]);
+  // });
 
 
 });
