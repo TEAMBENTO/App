fca6c4b846f67aeda61bcdc0e0fc7bc070228b9e
jeffrey lonergan <jrlone1990@gmail.com>
2018-06-25 14:30:30 -0700

    working on profile component

---
+++ src/components/profile/Profile.js
@@ -0,0 +1,15 @@
+// import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+// import { connect } from 'react-redux';
+
+// render() {
+
+//     const { activites, bio, demographic, location, image } = this.state
+//     return (
+
+
+//     ); 
+
+
+    
+// } 
