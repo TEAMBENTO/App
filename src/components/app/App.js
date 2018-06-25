7cfd9964c5034bfdb062a208780025d24b982dbb
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-06-25 13:25:44 -0700

    map displays on page

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 // import { connect } from 'react-redux';
 import Error from './Error';
 import Loading from './Loading';
+import Events from '../events/Events';
 
 
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
     return (
       <div>
         <h1>Hello World!</h1>
+        <Events/>
         <Error/>
         <Loading/>
       </div>
---
+++ src/components/events/Events.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import MapContainer from './MapContainer';
+
+
+
+export default class Events extends Component {
+  render() {
+    return (
+      <div>
+        <MapContainer/>
+      </div>
+    );
+  } 
+}
