68d68fa482f5e893301dfebd79cff617ab696fdd
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-25 16:49:10 -0700

    group detail static app

---
+++ src/components/groups/GroupDetail.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class GroupDetail extends Component {
+  render() {
+    return (
+      <div>
+        hello group detail
+      </div>
+    );
+  }
+}
