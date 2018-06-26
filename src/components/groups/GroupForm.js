11570c936e3b47709844908f81f1cb89533c412a
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-26 07:28:51 -0700

    working on the group from

--- src/components/groups/GroupForm.js
+++ src/components/groups/GroupForm.js
@@ -4,16 +4,67 @@ import { connect } from 'react-redux';
 import { addGroup } from './actions';
 import { getGroups } from './reducers';
 
+const defaultState = {
+  teamName: '',
+  description: '',
+  type: '',
+  image: '',
+  private: '',
+};
+
 class GroupForm extends Component {
 
-  static PropTypes = {
+  static propTypes = {
     addGroup: PropTypes.func.isRequired
   };
 
+  static getDerivedStateFromProps({ group }, { edit }) {
+    if(edit) return null;
+
+    return {
+      edit: group ? { ...group } : { ...defaultState }
+    }; 
+  }
+
+  state = {
+    edit: null
+  };
+
+  handleChange = ({ target }) => {
+    this.setState(({ edit }) => {
+      return {
+        edit: {
+          ...edit,
+          [target.placeholder]: target.value
+        }
+      };
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.addGroup(this.state.edit);
+    this.setState({
+      edit: { ...defaultState }
+    });
+  };
+
   render() {
+    const { teamName, description, type, image } = this.state;
+
     return (
       <div>
-        hello group form.
+        <form onSubmit={this.handleSubmit}>
+          <input placeholder="teamName" value={teamName} onChange={this.handleChange}/>
+          <input placeholder="type" value={type} onChange={this.handleChange}/>
+          <input placeholder="image" value={image} onChange={this.handleChange}/>
+          <textarea placeholder="description" value={description} onChange={this.handleChange}/>
+          <label>
+            Private
+            <input type="checkbox" placeholder="private" value={this.state.private} onChange={this.handleChange}/>
+          </label>
+          <button type="submit">Add Group</button>
+        </form>
       </div>
     );
   }
