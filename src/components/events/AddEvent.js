d534501de9d18028be1198adb8e5f4e5243398f0
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-06-26 14:31:38 -0700

    create addevent form component

--- src/components/events/AddEvent.js
+++ src/components/events/AddEvent.js
@@ -1,18 +1,73 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { addEvent } from './actions';
+import { getEvents } from './reducers';
 
-
+const defaultState = {
+  eventName: '',
+  description: '',
+  type: '',
+  location: '',
+  timeStart: '',
+  timeEnd: ''
+};
 
 class AddEvent extends Component {
+
+  static propTypes = {
+    addEvent: PropTypes.func.isRequired
+  };
+
+  state = {
+    form: defaultState
+  };
+
+  handleChange = ({ target }) => {
+    this.setState(({ form }) => {
+      return {
+        form: {
+          ...form,
+          [target.name]: target.value
+        }
+      };
+    });
+  };
+
+  handleSubmit = event => {
+    const { addEvent } = this.props;
+    event.preventDefault();
+    addEvent(this.state.form);
+  };
+
   render() {
+
+    const { eventName, description, type, location, timeStart, timeEnd } = this.state.form;
+
     return (
       <div>
-        Add Event details form
+        <h3>Create an Event</h3>
+        <form onSubmit={this.handleSubmit}>
+          <label>Event Name:</label>
+          <input type="text" name="eventName" value={eventName} onChange={this.handleChange}/>
+          <label>Type of Activity:</label>
+          <input type="text" name="type" value={type} onChange={this.handleChange}/>
+          <label>Location:</label>
+          <input type="text" name="location" value={location} onChange={this.handleChange}/>
+          <label>Event Start:</label>
+          <input type="text" name="timeStart" value={timeStart} onChange={this.handleChange}/>
+          <label>Event End:</label>
+          <input type="text" name="timeEnd" value={timeEnd} onChange={this.handleChange}/>
+          <label>Description:</label>
+          <textarea name="description" value={description} onChange={this.handleChange}/>
+          <button type="submit">Create your Event</button>
+        </form>
       </div>
     );
   }
 }
 
 export default connect(
-  mapStateToProps,
+  state => ({ event: getEvents(state) }),
+  { addEvent }
 )(AddEvent);
