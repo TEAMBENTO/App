0853fb69808d15c0d72ef36af71f50a5435d9f34
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-26 09:37:41 -0700

    working on getting the update form to work on the group detail view

--- src/components/groups/GroupDetail.js
+++ src/components/groups/GroupDetail.js
@@ -3,8 +3,13 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getGroup } from './reducers';
 import { loadGroup } from './actions';
+import { GroupForm } from './GroupForm';
 
 class GroupDetail extends Component {
+
+  state = {
+    editing: false
+  };
 
   static propTypes = {
     group: PropTypes.object,
@@ -16,16 +21,42 @@ class GroupDetail extends Component {
     console.log(this.props.match.params.id);
     this.props.loadGroup(this.props.match.params.id);
   }
+
+  handleEdit = () => {
+    this.setState({ editing: true });
+  };
+
+  handleCancel = () => {
+    this.setState({ editing: false });
+  };
+
+  handleUpdate = data => {
+    this.props.onUpdate(data);
+    this.setState({ editing: false });
+  };
   
 
   render() {
-    const { teamName, image, description } = this.props.group;
+    const { editing } = this.state;
+    const { group } = this.prop;
+    const { teamName, image, description } = group;
 
     return (
       <div>
         <h1>{teamName}</h1>
         <img src={image}/>
         <p>{description}</p>
+        {editing || <button onClick={this.handleEdit}>✐</button>}
+        {editing && 
+          <div>
+            <GroupForm
+              label="Update"
+              group={group}
+              onComplete={this.handleUpdate}
+              onCancel={this.handleCancel}
+            />
+          </div>
+        }
       </div>
     );
   }
