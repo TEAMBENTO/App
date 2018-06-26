7b4edf5c88b11998f7067f6d8d7acdcd9369cba2
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-25 17:25:50 -0700

    created group detail

--- src/components/groups/GroupDetail.js
+++ src/components/groups/GroupDetail.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getGroup } from './reducers';
+import { loadGroup } from './actions';
 
-export default class GroupDetail extends Component {
+class GroupDetail extends Component {
+
+  static propTypes = {
+    group: PropTypes.object,
+    match: PropTypes.object,
+    loadGroup: PropTypes.func.isRequired
+  };
+
+  componentDidMount() {
+    console.log(this.props.match.params.id);
+    this.props.loadGroup(this.props.match.params.id);
+  }
+  
+
   render() {
+    const { teamName, image, description } = this.props.group;
+
     return (
       <div>
-        hello group detail
+        <h1>{teamName}</h1>
+        <img src={image}/>
+        <p>{description}</p>
       </div>
     );
   }
 }
+
+export default connect(
+  state => ({ 
+    group: getGroup(state)
+  }),
+  { loadGroup }
+)(GroupDetail);
