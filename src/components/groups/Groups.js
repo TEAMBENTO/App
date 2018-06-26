037c2815f1c69dcfb9443a7d730c5bfb31f4e8be
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-26 15:48:44 -0700

    redirect to group when new group is added

--- src/components/groups/Groups.js
+++ src/components/groups/Groups.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { loadGroups } from './actions';
 import { getGroups } from './reducers';
@@ -9,6 +9,11 @@ import GroupForm from './GroupForm';
 import { addGroup } from './actions';
 
 class Groups extends Component {
+
+  state = {
+    redirect: false,
+    newGroup: ''
+  };
 
   static propTypes = {
     loadGroups: PropTypes.func.isRequired,
@@ -20,13 +25,26 @@ class Groups extends Component {
     this.props.loadGroups();
   }
 
+  handleAdd = group => {
+    this.props.addGroup(group)
+      .then(({ payload }) => {
+        this.setState({
+          redirect: true,
+          newGroup: payload
+        });
+      });
+  };
+
   render() {
-    const { groups, addGroup } = this.props;
+    const { groups } = this.props;
+    const { redirect, newGroup } = this.state;
     if(!groups) return null;
+
+    if(redirect && newGroup) return <Redirect to={`/groups/${newGroup._id}`}/>;
 
     return (
       <div>
-        <GroupForm label="Add" onComplete={addGroup}/>
+        <GroupForm label="Add" onComplete={this.handleAdd}/>
         {groups.map(group => <Link key={group._id} to={`/groups/${group._id}`}> 
           <GroupThumbnail {...group}/>
         </Link>)}
