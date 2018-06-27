1a225ba5a590dc62fefd156be6ac3091cafa1c4c
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-06-27 10:15:01 -0700

    map inits on user's current location now!! :)

--- src/components/events/MapContainer.js
+++ src/components/events/MapContainer.js
@@ -13,7 +13,48 @@ export class MapContainer extends Component {
     showingInfoWindow: false,
     activeMarker: {},
     selectedPlace: {},
+    fields: {
+      location: {
+        lat: 45.51,
+        lng: -122.65
+      }
+    }
   };
+
+  async componentDidMount() {
+    const { lat, lng } = await this.getcurrentLocation();
+    this.setState(prev => ({
+      fields: {
+        ...prev.fields,
+        location: {
+          lat,
+          lng
+        }
+      },
+      currentLocation: {
+        lat,
+        lng
+      }
+    }));
+  }
+
+  getcurrentLocation() {
+    if(navigator && navigator.geolocation) {
+      return new Promise((resolve) => {
+        navigator.geolocation.getCurrentPosition(pos => {
+          const coords = pos.coords;
+          resolve({
+            lat: coords.latitude,
+            lng: coords.longitude
+          });
+        });
+      });
+    }
+    return {
+      lat: 0,
+      lng: 0
+    };
+  }
 
   onMarkerClick = (props, marker, e) =>
     this.setState({
@@ -22,36 +63,39 @@ export class MapContainer extends Component {
       showingInfoWindow: true
     });
 
-    onMapClicked = (props) => {
-      if(this.state.showingInfoWindow) {
-        this.setState({
-          showingInfoWindow: false,
-          activeMarker: null
-        });
-      }
-    };
-
-
-    render() {
-      return (
-        <Map google={this.props.google} zoom={14} initialCenter={this.props.defaultCoords}>
- 
-          <Marker
-            onClick={this.onMarkerClick}
-            title={'The marker`s title will appear as a tooltip.'}
-            name={'Colonel Summers Park'}
-            position={{  lat: 45.5155, lng: -122.6467  }} />
- 
-          <InfoWindow
-            marker={this.state.activeMarker}
-            visible={this.state.showingInfoWindow}>
-            <div>
-              <h1>{this.state.selectedPlace.name}</h1>
-            </div>
-          </InfoWindow>
-        </Map>
-      );
+  onMapClicked = (props) => {
+    if(this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
     }
+  };
+
+
+  render() {
+
+
+    return (
+      <Map google={this.props.google} zoom={14} initialCenter={this.state.fields.location}
+        center={this.state.fields.location}>
+ 
+        <Marker
+          onClick={this.onMarkerClick}
+          title={'The marker`s title will appear as a tooltip.'}
+          name={'Colonel Summers Park'}
+          position={{  lat: 45.5155, lng: -122.6467  }} />
+ 
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}>
+          <div>
+            <h1>{this.state.selectedPlace.name}</h1>
+          </div>
+        </InfoWindow>
+      </Map>
+    );
+  }
 }
  
 export default GoogleApiWrapper({
