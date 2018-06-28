4ee491d76a8640b4f8ee6c2eaba3169e34961922
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-06-27 20:03:54 -0700

    event detail can now be edited !! 🔥

--- src/components/events/EventDetail.js
+++ src/components/events/EventDetail.js
@@ -3,14 +3,27 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { getSingleEvent } from './reducers';
 import { loadEvent } from './actions';
+import AddEvent from './AddEvent';
 
 
 class EventDetail extends Component {
+
+  state = {
+    editing: false
+  };
 
   static propTypes = {
     match: PropTypes.object,
     loadEvent: PropTypes.func.isRequired,
     singleEvent: PropTypes.object.isRequired
+  };
+
+  handleEdit = () => {
+    this.setState({ editing: true });
+  };
+
+  handleCancel = () => {
+    this.setState({ editing: false });
   };
   
   componentDidMount() {
@@ -19,7 +32,8 @@ class EventDetail extends Component {
 
   render() {
     if(!this.props.singleEvent._id) return null;
-    const { attendance, description, group, host, location, name, time, type } = this.props.singleEvent;
+    const { editing } = this.state;
+    const { attendance, description, group, host, location, name, time, type, _id } = this.props.singleEvent;
 
     const { start, end } = time;
 
@@ -28,7 +42,8 @@ class EventDetail extends Component {
 
     return (
       <div>
-        <h2>{name}</h2>
+        <h2>{name}</h2>{editing || <button onClick={this.handleEdit}>✐</button>}
+        {editing && <AddEvent editing={editing} id={_id} />}
         {!host ? <p>Hosted by: {host}</p> : null}
         {!group ? <p>Team: {group}</p> : null}
         <p>Activity: {type}</p>
