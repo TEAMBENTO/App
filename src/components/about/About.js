55d03a21dd2130e01e14eab0c3774f9038c51dad
Steele Walston <steele.walston@gmail.com>
2018-06-29 10:34:42 -0700

    background color changed on groups

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -33,28 +33,28 @@ class About extends PureComponent {
         <div className="each-person">
           <img src="https://avatars3.githubusercontent.com/u/16010116?s=460&v=4"/>
           <div className="info">
-            <p>This is Steph</p>
+            <p>Stephanie Smith</p>
             <a href="https://github.com/stephaniesmith"><GithubCircleIcon color="black"/></a>
           </div>
         </div>
         <div className="each-person">
           <img src="https://i.imgur.com/eAfLGvz.jpg"/>
           <div className="info">
-            <p>This is Henry</p>
+            <p>Henry Oliveira</p>
             <a href="https://github.com/hnrzzle"><GithubCircleIcon color="black"/></a>
           </div>
         </div>
         <div className="each-person">
           <img src="https://avatars2.githubusercontent.com/u/34801846?s=400&u=d624a01194d0aaa6e839eb375cc25c724ba50cd6&v=4"/>
           <div className="info">
-            <p>This is Steele</p>
+            <p>Steele Walston</p>
             <a href="https://github.com/SteeleWalston"><GithubCircleIcon color="black"/></a>
           </div>
         </div>
         <div className="each-person">
           <img src="https://avatars3.githubusercontent.com/u/23265017?s=400&u=d5fd3e7799176897c3413368151792441a76c8b2&v=4"/>
           <div className="info">
-            <p>This is Jeff</p>
+            <p>Jeff Lonergan</p>
             <a href="https://github.com/J3ffcon1"><GithubCircleIcon color="black"/></a>
           </div>
         </div>
