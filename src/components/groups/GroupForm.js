528df9083451f1cb054b0bf9cf2ded1841131abd
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-06-28 18:20:47 -0700

    drop down form

--- src/components/groups/GroupForm.js
+++ src/components/groups/GroupForm.js
@@ -35,6 +35,7 @@ class GroupForm extends Component {
   };
 
   handleChange = ({ target }) => {
+    console.log(this.state);
     this.setState(({ edit }) => {
       return {
         edit: {
@@ -43,6 +44,17 @@ class GroupForm extends Component {
           captains: [this.props.userProfile._id],
           members: [this.props.userProfile._id],
           private: target.checked ? true : false
+        }
+      };
+    });
+  };
+
+  handleSelect = ({ target }) => {
+    this.setState(({ edit }) => {
+      return {
+        edit: {
+          ...edit,
+          type: target.value
         }
       };
     });
@@ -57,19 +69,27 @@ class GroupForm extends Component {
   };
 
   render() {
-    const { teamName, description, type, image } = this.state.edit;
+    const categories = ['basketball', 'yoga', 'baseball', 'tennis', 'hiking', 'running', 'racquetball', 'frisbee', 'climbing', 'rafting', 'kayaking', 'swimming', 'golfing', 'football', 'ice hockey', 'volleyball', 'cross fit', 'softball', 'badminton', 'walking', 'chess', 'soccer'];
+    const { teamName, description, image } = this.state.edit;
     const { label, onCancel } = this.props;
 
     return (
       <div id = "group-form"><form onSubmit={this.handleSubmit}>
-        <input name="teamName" placeholder="Name" value={teamName} onChange={this.handleChange}/>
-        <input name="type" placeholder="Activity" value={type} onChange={this.handleChange}/>
-        <input name="image" placeholder="Image" value={image} onChange={this.handleChange}/>
-        <textarea name="description" placeholder="Description" value={description} onChange={this.handleChange}/>
-        <label>
+        <input name="teamName" placeholder="Name" value={teamName} onChange={this.handleChange} required/>
+        {/* <input name="type" placeholder="Activity" value={type} onChange={this.handleChange} required/> */}
+        <select onChange={this.handleSelect}>
+          <option>Activity</option>
+          {categories.map(category => <option key={category} value={category}>
+            {category}
+          </option>)
+          }
+        </select>
+        <input name="image" placeholder="Image" value={image} onChange={this.handleChange} required/>
+        <textarea name="description" placeholder="Description" value={description} onChange={this.handleChange} required/>
+        {/* <label>
           Private
           <input name="private" type="checkbox" onChange={this.handleChange}/>
-        </label>
+        </label> */}
         <button type="submit">{label}</button>
         {onCancel && <button type="reset" onClick={onCancel}>Cancel</button>}
       </form>
